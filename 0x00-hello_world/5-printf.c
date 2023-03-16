8018e8d3b01894b8731cedd579bb249fb357facf
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-03-16 01:47:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main - It will print a message using the printf function
  * Return: 0 (Succes)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
