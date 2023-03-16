be347fd559eafba70ddf039d35a2ad2f8a01f9da
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-03-16 03:50:43 -0700

    101.quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
- * Reurn: Always 1 (success)
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return 1;
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
