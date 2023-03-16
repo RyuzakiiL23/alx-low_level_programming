c407dec7e807f5f3385cc48f4ced127bf9651c83
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-03-16 01:32:53 -0700

    4'puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - It prints a message with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
