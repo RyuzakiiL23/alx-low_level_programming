f253ecb35a0106168b3ca2e1f46b8da31990a0dd
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-03-31 20:44:38 +0000

    rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,17 +9,17 @@
 
 void reverse_array(int *a, int n)
 {
-  int i;
+int i;
 
-  i = n-1;
-  while (i >= 0)
-  {
-    if (i != n-1)
-    {
-      printf(", ");
-    }
-    printf("%d", a[i]);
-    i--;
-  }
-  printf("\n");
+i = n - 1;
+while (i >= 0)
+{
+if (i != (n - 1))
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i--;
+}
+printf("\n");
 }
