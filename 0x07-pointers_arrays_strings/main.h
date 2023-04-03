45b4336e75469d0209be3f0389630d1407e02447
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-03 23:50:22 +0000

    set_string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-
- 
-
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,3 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+void set_string(char **s, char *to);
