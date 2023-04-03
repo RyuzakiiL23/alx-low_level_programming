206b442a2b2813352f7971fba12c7173eaea4d5e
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-03 22:01:59 +0000

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the 2D array of characters representing the chessboard
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
