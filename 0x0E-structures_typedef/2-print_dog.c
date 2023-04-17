cfdb15bbbbb92a86213e7580da000f19b7f60141
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-17 17:23:45 +0000

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+* print_dog - print struct d
+* @d: struct dog
+* Return : void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+		{
+			printf("Owmer: (nil)\n");
+		}
+		printf("Owner: %s\n", d->owner);
+	}
+}
