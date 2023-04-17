b8541bb6a4e09806e770c9aeb567e68f141902ce
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-17 15:58:19 +0000

    Woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+	* struct dog - stores dogs informations
+	* @name : stores dog's name
+	* @age : stores dog's age
+	* @owner : stores dog's owner's name
+	*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
