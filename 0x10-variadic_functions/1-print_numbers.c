07b3b4fb6d81de6470142ff124edd5b9e02578fd
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-04-20 21:42:59 +0000

    print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+ #include "variadic_functions.c"
+
+ /**
+ * print_numbers - print nubers followed by a new line
+ * @separator : a constant string separing intigers
+ * @n : nuber of intigers
+ * Return: Always (0)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int x;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		printf("%d", x);
+	}
+	if (separator != NULL && i < n - i)
+	{
+		printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+
+	return (0);
+}
