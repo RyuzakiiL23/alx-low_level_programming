b97cc4ec2a9e6e96a149f17ee75d2eb39e8f4f45
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-02 15:56:48 +0000

    nth_node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+* get_nodeint_at_index - free listint.
+* @head: node's head.
+* @index: index of node.
+* Return:  the nth node of a listint_t.
+*/
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
