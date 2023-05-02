add02c4e569d2b38fe6fc77f612315a9cf0fdef1
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-02 15:50:24 +0000

    nth_node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		i++;
+		head = head->next;
+	}
+	return (0);
+}
