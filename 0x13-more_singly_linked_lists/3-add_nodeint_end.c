a6baba640037c6c5ce66c4d5a5ad58359d50d7ed
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-02 10:11:36 +0000

    new_tail

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,10 +21,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 	{
-			*head = new_node;
+		*head = new_node;
 	}
 	else
-	{	
+	{
 		while (tmp->next != NULL)
 		{
 			tmp = tmp->next;
