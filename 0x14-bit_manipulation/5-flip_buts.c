464e3a0f405ce791ced6006d9ee6ef42238f9867
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-04 20:19:36 +0000

    flip_bits

--- 0x14-bit_manipulation/5-flip_buts.c
+++ 0x14-bit_manipulation/5-flip_buts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits to flip to get from one number to another
+ * flip_bits - returns the number of bits to flps
  * @n: first number
  * @m: second number
  *
@@ -9,14 +9,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
+	unsigned long int xor_result = n ^ m;
+	unsigned int count = 0;
 
-    while (xor_result)
-    {
-        count += xor_result & 1;
-        xor_result >>= 1;
-    }
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
 
-    return count;
+	return (count);
 }
