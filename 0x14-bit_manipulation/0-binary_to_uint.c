c747e4ae5d25778b3ffa7194aa7b25d57f3c4092
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-04 16:46:32 +0000

    bin_to_dec

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* binary_to_uint - convert a binary to decimal.
+* @b : a given binary.
+* return : the convergen.
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+	int len = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		result <<= 1;
+
+		if (b[i] == '1')
+			result += 1;
+	}
+
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <math.h>
+char _putchar(void);
+unsigned int binary_to_uint(const char *b);
+
+#endif
