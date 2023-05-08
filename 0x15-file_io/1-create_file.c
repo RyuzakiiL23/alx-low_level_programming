225df30aff406b473e849c94fd78b728abb74b6e
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-08 17:36:41 +0000

    creat_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_desc, n_letters, result;
+		int file_desc, n_letters, result;
 
-    if (!filename)
-        return (-1);
+		if (!filename)
+		return (-1);
 
-    file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+		file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-    if (file_desc == -1)
-        return (-1);
+		if (file_desc == -1)
+		return (-1);
 
-    if (!text_content)
-        text_content = "";
+		if (!text_content)
+		text_content = "";
 
-    for (n_letters = 0; text_content[n_letters]; n_letters++)
-        ;
+		for (n_letters = 0; text_content[n_letters]; n_letters++)
+		;
 
-    result = write(file_desc, text_content, n_letters);
+		result = write(file_desc, text_content, n_letters);
 
-    if (result == -1)
-        return (-1);
+		if (result == -1)
+		return (-1);
 
-    close(file_desc);
+		close(file_desc);
 
-    return (1);
+		return (1);
 }
