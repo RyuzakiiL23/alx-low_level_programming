a192740f2df4c489fc5fa35820d9a47c60f8e19c
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-05-08 17:28:35 +0000

    creat_file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 char _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
