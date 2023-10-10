4626ca9538e78c7ead68ac284c79710f36aaacc7
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-11 00:51:30 +0100

    jump_search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Searches for a value in an array using the Jump search algo
+ *
+ * @array: Pointer to the first element of the array
+ * @size: The size of the array
+ * @value: The value to search for
+ *
+ * Return: If value is found, return the index; otherwise, return -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int index, m, k, prev;
+	
+	if (array == NULL || size == 0)
+		return (-1);
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+	}
+
+	return (-1);
+}
