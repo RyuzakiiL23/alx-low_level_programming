592a06b18b7eea1af41d495a41644d120b36d92d
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-12 22:23:21 +0100

    104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,38 +1,6 @@
 #include "search_algos.h"
 
 /**
- * rec - Recursive function to find the first index where 'value' is located.
- * @middle: The current index in the 'array' being checked.
- * @array: Pointer to the array of integers.
- * @value: The value to search for.
- *
- * Return: The first index where 'value' is located, or 'middle' if not found.
- */
-int rec(int left, int middle, int *array, int value)
-{
-	int new_mid = middle - 1;
-	int i = 0;
-
-	printf("Searching in array: ");
-	for (i = left; i <= middle; i++)
-		if (i == left)
-			printf("%d", array[i]);
-		else
-			printf(", %d", array[i]);
-	printf("\n");
-
-	if (array[middle - 1] == value)
-	{
-		printf("Searching in array: ");
-		printf("%d", array[new_mid]);
-		printf(", %d", array[new_mid]);
-		printf("\n");
-		return (rec(left, new_mid, array, value));
-	}
-	return (middle);
-}
-
-/**
  * advanced_binary - Searches for a value in a sorted array using binary search
  * @array: Pointer to the first element of the array
  * @size: The size of the array
@@ -74,4 +42,36 @@ int advanced_binary(int *array, size_t size, int value)
 			right = middle - 1;
 	}
 	return (-1);
+}
+
+/**
+ * rec - Recursive function to find the first index where 'value' is located.
+ * @middle: The current index in the 'array' being checked.
+ * @array: Pointer to the array of integers.
+ * @value: The value to search for.
+ *
+ * Return: The first index where 'value' is located, or 'middle' if not found.
+ */
+int rec(int left, int middle, int *array, int value)
+{
+	int new_mid = middle - 1;
+	int i = 0;
+
+	printf("Searching in array: ");
+	for (i = left; i <= middle; i++)
+		if (i == left)
+			printf("%d", array[i]);
+		else
+			printf(", %d", array[i]);
+	printf("\n");
+
+	if (array[middle - 1] == value)
+	{
+		printf("Searching in array: ");
+		printf("%d", array[new_mid]);
+		printf(", %d", array[new_mid]);
+		printf("\n");
+		return (rec(left, new_mid, array, value));
+	}
+	return (middle);
 }
